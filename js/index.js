a1fbbca4f2755631ece2888f6f01a390d69a7fbb
rifathosenofficial <rifathosenofficial@gmail.com>
2023-06-15 16:14:18 +0600

    javascript code added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 document.getElementById('btn-not').addEventListener('click',function(){
     alert("Hello\n I'm so sorry .This website is not finished yet.");
 });
+document.getElementById('cv-info').addEventListener('click',function(){
+    alert("CV not added\n I'm so sorry .This website is not finished yet.");
+});
